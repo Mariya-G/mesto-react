c4b731dee5a10849abac959da7f740a5e5dc2bd1
Maria Gorodilova <mashunja13@yandex.ru>
2023-07-03 14:47:31 +0500

    React правки 1 интерации

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,54 +36,56 @@ function App() {
   }
 
   return (
-    <body className="page">
-    <Header />
-    <Main 
-      onEditAvatar={handleEditAvatarClick}
-      onEditProfile={handleEditProfileClick}
-      onAddPlace={handleAddPlaceClick}
-      onCardClick={handleCardClick}
-    />
-    <Footer />
-    <PopupWithForm
-      isOpen={isEditProfilePopupOpen}
-      onClose={closeAllPopups}
-      title = {'Редактировать профиль'}
-      name = {'edit'}
-      buttonText = {'Сохранить'}
-    >
-      <input id="name" name="username" className="popup__input popup__input_type_name" type="text" placeholder="Имя" value="" required minlength="2" maxlength="40"/>
-      <span className="name-error popup__input-error"></span>
-      <input id="job" name="job" className="popup__input popup__input_type_job" type="text" placeholder="О себе" value="" required minlength="2" maxlength="200"/>
-      <span className="job-error popup__input-error"></span>
-    </PopupWithForm>
-    <PopupWithForm
-      isOpen={isAddPlacePopupOpen}
-      onClose={closeAllPopups}
-      title = {'Редактировать профиль'}
-      name = {'add'}
-      buttonText = {'Создать'}
-    >
-      <input id="input-name" name="name" className="popup__input popup__input_type_title" type="text" placeholder="Название" value="" required minlength="2" maxlength="30" />
-      <span className="input-name-error popup__input-error"></span>
-      <input id="input-link" name="link" className="popup__input popup__input_type_link" type="url" placeholder="Ссылка на картинку" value="" required />
-      <span className="input-link-error popup__input-error"></span>
-    </PopupWithForm>
-    <PopupWithForm
-      isOpen={isEditAvatarPopupOpen}
-      onClose={closeAllPopups}
-      title = {'Обновить аватар'}
-      name = {'avatar'}
-      buttonText = {'Сохранить'}
-    >
-      <input id="avatar" name="avatar" className="popup__input popup__input_type_link" type="url" placeholder="Ссылка на картинку" value="" required />
-      <span className="avatar-error popup__input-error"></span>
-    </PopupWithForm>
-    <ImagePopup
-      card={selectedCard}
-      onClose={closeAllPopups}
-     />
-  </body>
+    <div className="pages">
+      <div className="page">
+      <Header />
+      <Main 
+        onEditAvatar={handleEditAvatarClick}
+        onEditProfile={handleEditProfileClick}
+        onAddPlace={handleAddPlaceClick}
+        onCardClick={handleCardClick}
+      />
+      <Footer />
+      <PopupWithForm
+        isOpen={isEditProfilePopupOpen}
+        onClose={closeAllPopups}
+        title = 'Редактировать профиль'
+        name = {'edit'}
+        buttonText = 'Сохранить'
+      >
+        <input id="name" name="username" className="popup__input popup__input_type_name" type="text" placeholder="Имя" value="" required minLength="2" maxLength="40"/>
+        <span className="name-error popup__input-error"></span>
+        <input id="job" name="job" className="popup__input popup__input_type_job" type="text" placeholder="О себе" value="" required minLength="2" maxLength="200"/>
+        <span className="job-error popup__input-error"></span>
+      </PopupWithForm>
+      <PopupWithForm
+        isOpen={isAddPlacePopupOpen}
+        onClose={closeAllPopups}
+        title = 'Редактировать профиль'
+        name = {'add'}
+        buttonText = 'Создать'
+      >
+        <input id="input-name" name="name" className="popup__input popup__input_type_title" type="text" placeholder="Название" value="" required minLength="2" maxLength="30" />
+        <span className="input-name-error popup__input-error"></span>
+        <input id="input-link" name="link" className="popup__input popup__input_type_link" type="url" placeholder="Ссылка на картинку" value="" required />
+        <span className="input-link-error popup__input-error"></span>
+      </PopupWithForm>
+      <PopupWithForm
+        isOpen={isEditAvatarPopupOpen}
+        onClose={closeAllPopups}
+        title = 'Обновить аватар'
+        name = {'avatar'}
+        buttonText = 'Сохранить'
+      >
+        <input id="avatar" name="avatar" className="popup__input popup__input_type_link" type="url" placeholder="Ссылка на картинку" value="" required />
+        <span className="avatar-error popup__input-error"></span>
+      </PopupWithForm>
+      <ImagePopup
+        card={selectedCard}
+        onClose={closeAllPopups}
+      />
+    </div>
+  </div>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,20 +6,18 @@ const Card = (props) => {
   }
   
   return (
-  <>
-  <article className="element">
-    <img name="name" onClick={handleClick} className="element__image" src={card.link} alt={card.name}/>
-      <button className="element__delete"></button>
-      <div className="element__wrap">
-        <h2 className="element__title">{card.name}</h2>
-        <div className="element__wrap-likes">
-          <button type="button" className="element__like"></button>
-          <span className="element__likes-number">{card.likes.length}</span>
+    <article className="element">
+      <img name="name" onClick={handleClick} className="element__image" src={card.link} alt={card.name}/>
+        <button className="element__delete"></button>
+        <div className="element__wrap">
+          <h2 className="element__title">{card.name}</h2>
+          <div className="element__wrap-likes">
+            <button type="button" className="element__like"></button>
+            <span className="element__likes-number">{card.likes.length}</span>
+          </div>
         </div>
-      </div>
     </article>
-  </>
   )
-  
 }
+
 export default Card;
